2378fb3d08937f315fba97c3543f021c72f8ef6e
carlos-henriquep <krlos.henrique98@gmail.com>
2024-06-06 20:11:52 -0300

    Feature/ Class Calculator

--- src/Classe/Calculator.ts
+++ src/Classe/Calculator.ts
@@ -1,3 +1,40 @@
-class Calculator {
-    
+export class Calculator {
+    private _numA: number;
+    private _numB: number;
+
+
+    public getNumA(): number{
+        return this._numA;
+    }
+
+    public getNumB(): number{
+        return this._numB;
+    }
+
+   public setNumA(num: number): void{
+    this._numA = num;
+   }
+
+   public setNumB(num: number): void{
+    this._numB = num;
+   }
+
+   public sum(): number{
+    return this._numA + this._numB;
+   }
+
+   public subtraction(): number {
+    return this._numA - this._numB
+   }
+
+   public multiplication(): number {
+    return this._numA * this._numB
+   }
+
+   public division(): number {
+    if(this._numB === 0){
+        throw new Error("Divisão por zero não é permitida")
+    }
+    return this._numA / this._numB
+   }
 }
